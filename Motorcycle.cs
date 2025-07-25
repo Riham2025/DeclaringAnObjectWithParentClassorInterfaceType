57918135524cbe2fde246f2a157a28c3deb60902
Riham2025 <riham91.alsiyabi@gmail.com>
2025-07-25 21:30:50 +0400

    Declaring an object of type IVehicle with a Motorcycle instance

--- Motorcycle.cs
+++ Motorcycle.cs
@@ -9,5 +9,6 @@ namespace DeclaringAnObjectWithParentClassorInterfaceType
     class Motorcycle : IVehicle
     {
         public void Start() => Console.WriteLine("Motorcycle is starting..."); // Implementation of the Start method from the IVehicle interface
+        IVehicle v2 = new Motorcycle(); // Declaring an object of type IVehicle with a Motorcycle instance
     }
 }
