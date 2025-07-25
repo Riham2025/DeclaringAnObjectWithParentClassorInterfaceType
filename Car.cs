2a0143299ca0b1f7350ecd5ecf071442ecb564d8
Riham2025 <riham91.alsiyabi@gmail.com>
2025-07-25 21:25:13 +0400

    create new class call car

---
+++ Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclaringAnObjectWithParentClassorInterfaceType
+{
+    class Car : IVehicle
+    {
+        public void Start() => Console.WriteLine("Car is starting...");
+    }
+}
