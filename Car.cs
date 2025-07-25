72f026f5ff610438176c5d2e734245821518350a
Riham2025 <riham91.alsiyabi@gmail.com>
2025-07-25 21:29:59 +0400

    Declaring an object of type IVehicle with a Car instance

--- Car.cs
+++ Car.cs
@@ -9,5 +9,8 @@ namespace DeclaringAnObjectWithParentClassorInterfaceType
     class Car : IVehicle // This class implements the IVehicle interface
     {
         public void Start() => Console.WriteLine("Car is starting..."); // Implementation of the Start method from the IVehicle interface
+
+        IVehicle v1 = new Car(); // Declaring an object of type IVehicle with a Car instance
     }
+
 }
